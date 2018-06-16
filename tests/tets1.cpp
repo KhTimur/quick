8bc6cc86c1a62f5311ceab49d431a4d3273f49bd
KhTimur <khazirov@yandex.ru>
2018-06-16 13:45:39 +0300

    finish

--- tests/tets1.cpp
+++ tests/tets1.cpp
@@ -5,7 +5,7 @@
 TEST_CASE("quick test") {
 
 	int nums[] = { 9,1,7,2,5,8,3,6,4 };
-	int nums_proverka[] = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+	int nums_proverka[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	int *begi = std::begin(nums);
 	int *en = std::end(nums);
 	quick_sort(begi, en);
